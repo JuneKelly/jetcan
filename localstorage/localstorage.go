e2a5f3484d6dfd94bc4ba2155b2e8e4de6b1c78b
Shane Kilkelly <shane@kilkelly.me>
2014-06-29 16:11:40 +0100

    start work around the users local storage directory

---
+++ localstorage/localstorage.go
@@ -0,0 +1,82 @@
+package localstorage
+
+import (
+	"os"
+	"os/user"
+	"path/filepath"
+	"fmt"
+)
+
+const DIRECTORY	string = ".jetcan.d"
+
+func userHomeDir() (string, error) {
+	u, err := user.Current()
+	if err != nil {
+		return "", err
+	} else {
+		return u.HomeDir, nil
+	}
+}
+
+func exists() (bool, error) {
+	var homeDir	string
+	var path	string
+	var err		error
+
+	homeDir, err = userHomeDir()
+	if err != nil {
+		return false, err
+	}
+
+	path = filepath.Join(homeDir, DIRECTORY)
+
+    _, err = os.Stat(path)
+    if err == nil {
+		return true, nil
+	}
+    if os.IsNotExist(err) {
+		return false, nil
+	}
+    return false, err
+}
+
+func createStorageDir() error {
+	var homeDir	string
+	var path	string
+	var err		error
+
+	homeDir, err = userHomeDir()
+	if err != nil {
+		return err
+	}
+	path = filepath.Join(homeDir, DIRECTORY)
+
+	fmt.Println("Creating local storage direcotory", path)
+
+	err = os.Mkdir(path, os.ModeDir)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Initialize() error {
+	var storageExists	bool
+	var err				error
+
+	storageExists, err = exists()
+	if err != nil {
+		return err
+	}
+
+	if !storageExists {
+		err = createStorageDir()
+		if err != nil {
+			return err
+		} else {
+			return nil
+		}
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -2,23 +2,34 @@ package main
 
 import (
 	"github.com/codegangsta/cli"
+	"github.com/ShaneKilkelly/jetcan/localstorage"
 	"fmt"
 	"os"
 )
 
+func handler(c *cli.Context) {
+	fmt.Println("this is jetcan")
+}
 
-func initApp() (app *cli.App) {
+func initApp() (app *cli.App, err error) {
 	app = cli.NewApp()
 
 	app.Name = "jetcan"
 	app.Version = "0.1.0"
-	app.Action = func (c *cli.Context) {
-		fmt.Println("this is jetcan")
+	app.Action = handler
+
+	err = localstorage.Initialize()
+	if err != nil {
+		return nil, err
 	}
-	return app
+	return app, nil
 }
 
 func main() {
-	app := initApp()
-	app.Run(os.Args)
+	app , err := initApp()
+	if err != nil {
+		fmt.Println("ERROR", err)
+	} else {
+		app.Run(os.Args)
+	}
 }
